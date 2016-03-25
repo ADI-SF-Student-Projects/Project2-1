876812254482c670f7ef178118d2c0c8050dbf65
Nicolas Saad <nicolassaad@Nicolass-MacBook-Pro.local>
2016-03-25 15:39:48 -0700

    Project 2: 2nd Final Version, with refactored code using classes to populate database, and with junit test case for the the getters and setters class that gets and sets the database data

---
+++ NeighborhoodGuideAppV6/app/src/main/java/com/example/nicolassaad/neighborhoodguideapp/BarItem.java
@@ -0,0 +1,81 @@
+package com.example.nicolassaad.neighborhoodguideapp;
+
+/**
+ * Created by nicolassaad on 3/25/16.
+ */
+public class BarItem {
+
+    public int primaryKey;
+    public String name;
+    public String address;
+    public String description;
+    public float rating;
+    public String price;
+    public int favorite;
+    public int imageResourceID;
+
+
+    public int getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public void setPrimaryKey(int primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(int favorite) {
+        this.favorite = favorite;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+
+    public void setImageResourceID(int imageResourceID) {
+        this.imageResourceID = imageResourceID;
+    }
+}
---
+++ NeighborhoodGuideAppV6/readme.md
@@ -0,0 +1,16 @@
+ Nicolas Saad - Project 2
+PubCrawl - The App for Bars
+—————————————————————————
+
+This app lets the user search for different bars in the Berkeley neighborhood.
+The user can search using one of three different search criteria.
+Bars can be stored in a favorites list for easy access.
+—————————————————————————
+
+Bugs:
+-In the first screen, the user cannot select different cities at the moment
+-The user can only search for bars by using only one of the three search criteria
+The beer.
+-In the search results activity the Beer button in the action bar does not take the 
+user back home like it’s suppose to. 
+
